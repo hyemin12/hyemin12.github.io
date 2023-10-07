dcf98e41ff6e5a22359f3b7d91db40a7eb7360fb
hyemin12 <khnm31@naver.com>
2023-10-07 18:46:20 +0900

    rollup.config초기화

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import license from 'rollup-plugin-license';
 import path from 'path';
 
 const JS_SRC = '_javascript';
-const JS_DIST = '';
+const JS_DIST = 'assets/js/dist';
 const isProd = process.env.NODE_ENV === 'production';
 
 function build(filename) {
